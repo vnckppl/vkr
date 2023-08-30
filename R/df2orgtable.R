196340963b230cb6df246376ef34a1ca1e822b34
vnckppl <vincent.koppelmans@utah.edu>
2023-08-30 14:51:45 -0600

    Fix for index columns that were colorized twice
    Fix for index columns that were colorized twice if multiple p-values were
    surpassing the threshold in the rows (this could happen when there was a
    categorical predictor with more than two levels).

--- R/df2orgtable.R
+++ R/df2orgtable.R
@@ -1,8 +1,8 @@
 #' Convert a data frame to an org table and print it
 #'
 #' @param df Name of a data element in your working environment
-#' @param colorizep Find columns ending in 'p' or 'fdr' and color the values
-#' smaller than the threshold
+#' @param colorizep When set to 'auto', it will find columns ending in 'p' or
+#' 'fdr' and color the values smaller than the threshold
 #' @param thold Threshold for coloring
 #' @export
 df2orgtable <- function(df, colorizep = FALSE, thold = 0.05) {
@@ -21,7 +21,7 @@ df2orgtable <- function(df, colorizep = FALSE, thold = 0.05) {
         ## *** Loop over p-value columns
         pcolors <- c("to1", "to2", "to3")
         for (npcol in seq_len(length(pcols))) {
-            
+
             ## **** Put information into variables
             pcol <- pcols[npcol]   # Column name
             tcol <- pcolors[npcol] # Color to be used
@@ -30,16 +30,21 @@ df2orgtable <- function(df, colorizep = FALSE, thold = 0.05) {
             prefix <- paste0("¡", tcol, "¡")
             postfix <- paste0("¡/", tcol, "¡")
 
-            ## **** Apply the coloring to the columns 
+            ## **** Apply the coloring to the columns
             df[[pcol]][df[[pcol]] < thold] <-
                 paste0(prefix, df[[pcol]][df[[pcol]] < thold], postfix)
 
             ## **** Also colorize the first two columns
             # Colorize the first two columns if the pvalue for that row is
-            # surpassing the treshold.
-            df[[1]][df[[pcol]] < thold] <-
-                   paste0("¡ty1¡", df[[1]][df[[pcol]] < thold], "¡/ty1¡")
-            df[[2]][df[[pcol]] < thold] <-
+            # surpassing the treshold. Make sure to only do this once (so to
+            # not color the same variable twice, because mutiple p-values on
+            # the row are surpassing the threshold).
+            ## ***** Outcome measure
+            df[[1]][df[[pcol]] < thold & !grep("¡", df[[1]])] <-
+                    paste0("¡ty1¡", df[[1]][df[[pcol]] < thold], "¡/ty1¡")
+
+            ## ***** Predictor
+            df[[1]][df[[pcol]] < thold & !grep("¡", df[[2]])] <-
                    paste0("¡ty2¡", df[[2]][df[[pcol]] < thold], "¡/ty2¡")
         }
 
@@ -60,32 +65,34 @@ df2orgtable <- function(df, colorizep = FALSE, thold = 0.05) {
             prefix <- paste0("¡", tcol, "¡")
             postfix <- paste0("¡/", tcol, "¡")
 
-            ## **** Apply the coloring to the columns 
+            ## **** Apply the coloring to the columns
             df[[fdrcol]][df[[fdrcol]] < thold] <-
                 paste0(prefix, df[[fdrcol]][df[[fdrcol]] < thold], postfix)
 
         }
 
-    } else if (! FALSE) {
-
-        print("I have left room here to add code to have users add a list
-of column name and color pairs to define which columns should be colored how.")
-        
+    } else if (FALSE) {
+        print(paste(
+            "I have left room here to add code to have users add a list of",
+            "column name and color pairs to define which columns should be",
+            "colored how."
+        ))
     }
-    
+
     ## * Header of the data frame
     my_hdr <- paste(names(df), collapse = "|") |>
-        gsub("^", "|", x = _) |> gsub("$", "|", x = _)
+        gsub("^", "|", x = _) |>
+        gsub("$", "|", x = _)
 
     ## * Body of the data frame
     my_body <- apply(df[, names(df)], 1, paste, collapse = "|") |>
-        gsub("^", "|", x = _) |> gsub("$", "|", x = _)
+        gsub("^", "|", x = _) |>
+        gsub("$", "|", x = _)
 
     ## * Combine data
     my_out <- c(my_hdr, "|-", my_body)
 
     ## * Print
     cat(my_out, sep = "\n")
-    ## return(my_out)
 
 }
